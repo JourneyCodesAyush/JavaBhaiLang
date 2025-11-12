946a2d602e1766771c966764b22668af782b9157
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2025-11-12 20:07:05 +0530

    feat(parser): add Parser to parse the list of Tokens and generate the AST
    Handles basic expressions and variable declaration as well as statement printing.

---
+++ src/main/java/io/github/journeycodesayush/javabhailang/parser/Parser.java
@@ -0,0 +1,254 @@
+package io.github.journeycodesayush.javabhailang.parser;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import static io.github.journeycodesayush.javabhailang.lexer.TokenType.*;
+
+import io.github.journeycodesayush.javabhailang.BhaiLang;
+import io.github.journeycodesayush.javabhailang.lexer.Token;
+import io.github.journeycodesayush.javabhailang.lexer.TokenType;
+
+public class Parser {
+    private static class ParseError extends RuntimeException {
+    }
+
+    private final List<Token> tokens;
+    private int current = 0;
+
+    public Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    public List<Stmt> parse() {
+        List<Stmt> statements = new ArrayList<>();
+        while (!isAtEnd()) {
+            statements.add(declaration());
+        }
+        return statements;
+    }
+
+    private Stmt declaration() {
+        try {
+            if (match(BHAI_YE_HAI))
+                return varDeclaration();
+
+            return statement();
+        } catch (ParseError error) {
+            synchronize();
+            return null;
+        }
+    }
+
+    private Stmt varDeclaration() {
+        Token name = consume(IDENTIFIER, "Expect variable name.");
+
+        Expr initializer = null;
+        if (match(EQUAL)) {
+            initializer = expression();
+        }
+        consume(SEMICOLON, "Expect ';' after variable declaration.");
+        return new Stmt.Var(name, initializer);
+    }
+
+    private Stmt statement() {
+        if (match(BOL_BHAI)) {
+            return printStatement();
+        }
+        if (match(LEFT_CURLY_BRACE))
+            return new Stmt.Block(block());
+        return expressionStatement();
+    }
+
+    private Stmt printStatement() {
+        Expr value = expression();
+        consume(SEMICOLON, "Expect ';' after value.");
+        return new Stmt.Print(value);
+    }
+
+    private Stmt expressionStatement() {
+        Expr expr = expression();
+        consume(SEMICOLON, "Expect ';'after value.");
+        return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(RIGHT_CURLY_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_CURLY_BRACE, "Expect '}' after block.");
+        return statements;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private boolean isAtEnd() {
+        return peek().getType() == EOF;
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) {
+            current++;
+        }
+        return previous();
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) {
+            return false;
+        }
+        return peek().getType() == type;
+    }
+
+    private Expr expression() {
+        return assignment();
+    }
+
+    private Expr assignment() {
+        Expr expr = equality();
+
+        if (match(EQUAL)) {
+            Token equals = previous();
+            Expr value = assignment();
+
+            if (expr instanceof Expr.Variable) {
+                Token name = ((Expr.Variable) expr).name;
+                return new Expr.Assign(name, value);
+            }
+            error(equals, "Invalid assignment target.");
+        }
+
+        return expr;
+    }
+
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr comparison() {
+        Expr expr = term();
+
+        while (match(GREATER_EQUAL, GREATER, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr term() {
+        Expr expr = factor();
+
+        while (match(PLUS, MINUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr factor() {
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr unary() {
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+        return primary();
+    }
+
+    private Expr primary() {
+        if (match(BOOLEAN))
+            return new Expr.Literal(previous().getLiteral());
+
+        if (match(NALLA))
+            return new Expr.Literal(null);
+
+        if (match(STRING, NUMBER))
+            return new Expr.Literal(previous().getLiteral());
+
+        if (match(IDENTIFIER))
+            return new Expr.Variable(previous());
+
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect a ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+
+        throw error(peek(), "Expect expression.");
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type))
+            return advance();
+        throw error(peek(), message);
+    }
+
+    private ParseError error(Token token, String message) {
+        BhaiLang.error(token, message);
+        return new ParseError();
+    }
+
+    private void synchronize() {
+        advance();
+
+        while (!isAtEnd()) {
+            if (previous().getType() == SEMICOLON) {
+                return;
+            }
+            switch (peek().getType()) {
+                case BHAI_YE_HAI:
+                case BOL_BHAI:
+                case AGAR_BHAI:
+                case WARNA_BHAI:
+                case NAHI_TO_BHAI:
+                case JAB_TAK_BHAI:
+                case BAS_KAR_BHAI:
+                case AGLA_DEKH_BHAI:
+                    return;
+
+                default:
+                    break;
+            }
+            advance();
+        }
+    }
+
+}
