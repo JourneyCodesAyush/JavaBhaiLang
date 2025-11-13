8e5e8061882326114a65933d02f51f731ed57792
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2025-11-13 10:56:18 +0530

    feat(interpreter): add Environment.java for variable scopes and storage
    Manages block scopes and stores variables with their associated data

---
+++ src/main/java/io/github/journeycodesayush/javabhailang/interpreter/Environment.java
@@ -0,0 +1,46 @@
+package io.github.journeycodesayush.javabhailang.interpreter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.github.journeycodesayush.javabhailang.lexer.Token;
+
+public class Environment {
+    final Environment enclosing;
+    private final Map<String, Object> values = new HashMap<>();
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
+
+    Object get(Token name) {
+        if (values.containsKey(name.getLexeme())) {
+            return values.get(name.getLexeme());
+        }
+
+        if (enclosing != null)
+            return enclosing.get(name);
+
+        throw new RuntimeError(name, "Undefined variable '" + name.getLexeme() + "'.");
+    }
+
+    void define(String name, Object value) {
+        values.put(name, value);
+    }
+
+    void assign(Token name, Object value) {
+        if (values.containsKey(name.getLexeme())) {
+            values.put(name.getLexeme(), value);
+            return;
+        }
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+        throw new RuntimeError(name, "Undefined variable '" + name.getLexeme() + "'.");
+    }
+}
