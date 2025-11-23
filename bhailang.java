6d81dd38f9aaccb868d7ce0316512c2864ffed61
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2025-11-23 20:26:48 +0530

    chore: add launcher for JBang CLI command

---
+++ bhailang.java
@@ -0,0 +1,12 @@
+//JAVA 21
+//REPOS https://jitpack.io
+//DEPS com.github.journeycodesayush:javabhailang:v0.3.0
+//JAVAC_OPTIONS --release 21
+
+import io.github.journeycodesayush.javabhailang.BhaiLang;
+
+public class bhailang {
+    public static void main(String[] args) throws Exception {
+        BhaiLang.main(args);
+    }
+}
