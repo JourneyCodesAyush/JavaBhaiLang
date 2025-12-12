14087e9f14ca583066728bd386128a35b2d89150
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2025-12-12 12:21:54 +0530

    chore: bump //DEPS to latest BhaiLang version v0.7.0

--- bhailang.java
+++ bhailang.java
@@ -1,6 +1,6 @@
 //JAVA 21
 //REPOS https://jitpack.io
-//DEPS com.github.journeycodesayush:javabhailang:v0.6.0
+//DEPS com.github.journeycodesayush:javabhailang:v0.7.0
 //JAVAC_OPTIONS --release 21
 
 import io.github.journeycodesayush.javabhailang.BhaiLang;
